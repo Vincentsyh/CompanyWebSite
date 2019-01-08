7b53fdac23cbb7fe7a7e08dba5ce1a2e8acde732
苏一豪 <Suyihao@AppledeMacBook-Pro.local>
2019-01-08 18:16:20 +0800

    增加ProductDao功能和ProductEntity实体

--- src/dao/ProductDao.java
+++ src/dao/ProductDao.java
@@ -1,10 +1,31 @@
 package dao;
 
 import entity.ProductEntity;
+import util.Jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ProductDao {
-
+    ResultSet rs = null;
+    ArrayList<ProductEntity> products = new ArrayList<ProductEntity>();
+    public ArrayList<ProductEntity> getProductPic(int id) throws SQLException, ClassNotFoundException {
+        Connection conn = Jdbc.connect();
+        PreparedStatement state = null;
+        state = conn.prepareStatement("SELECT * FROM product WHERE `type`=?");
+        state.setInt(1,id);
+        rs = state.executeQuery();
+        while (rs.next()) {
+            ProductEntity product = new ProductEntity();
+            product.setId(rs.getString("id"));
+            product.setImg(rs.getString("img"));
+            products.add(product);
+        }
+        rs.close();
+        Jdbc.close(state,conn);
+        return products;
+    }
 }
--- src/entity/ProductEntity.java
+++ src/entity/ProductEntity.java
@@ -1,7 +1,46 @@
 package entity;
 
-
+import java.util.Date;
 
 public class ProductEntity {
-
+    //产品编号
+    private String id;
+    //产品图片
+    private String img;
+    //上架日期
+    private Date update;
+    //产品分类
+    private String type;
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public String getId(){
+        return id;
+    }
+    
+    public void setImg(String img){
+        this.img = img;
+    }
+    
+    public String getImg(){
+        return img;
+    }
+    
+    public void setUpdate(Date update){
+        this.update = update;
+    }
+    
+    public Date getUpdate(){
+        return update;
+    }
+    
+    public void setType(String type){
+        this.type = type;
+    }
+    
+    public String getType(){
+        return type;
+    }
 }
